e4fefe909f2b0e72ceb39e7b87083b9189a71027
Fernando Zunino <fernando@zauberlabs.com>
2012-03-23 15:43:22 -0300

    New Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Delfos::Application.config.secret_token = '73aaaa7be6ed3c4107c8f6d637fc36aded9eedad2615ce8246dbf167aece945f8bcd0047afd46d365b26b3a8106c07d56fea0560f185df711920006f4c5a6e80'
