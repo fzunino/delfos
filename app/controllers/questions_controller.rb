3bb8cc912b56e29792e83b6ff425c3c0adf55523
Fernando Zunino <fernando@zauberlabs.com>
2012-03-23 20:30:39 -0300

    Respuesta en formato json de questions (/questions.json)

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,12 @@
 class QuestionsController < ApplicationController
   def index
+    @questions = Question.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render :json => @questions.to_json(:include => {:tags => {:only => :name}, :user => {}}, :methods => :total_answers, :exclude => [:user_id])}
+    end
+    
   end
   
   def unanswered
